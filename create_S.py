66cd55b1f0ee745a8daf8ffec06d0ca76a1e14d8
hetaoconquer <t77123645o@qq.com>
2017-04-19 10:17:55 +0800

    Create create_S.py

---
+++ create_S.py
@@ -0,0 +1,28 @@
+import numpy as np
+import scipy.io as sio
+import os
+
+data = np.load('cifar_KNN.npz')['topK']# cifar_KNN.npz is the first KNN rank ,the top K nearest neighbor of  each image
+K1 = 20
+K2 = 30
+S = np.ones((size,size))*-1 
+for i in xrange(size):
+    top = set(data[i][:K1+1]) #exclude ifself so add one
+    nums=[]
+    idx = []
+    for j in xrange(size):
+         si = set(data[j][:K1+1])
+         both = si & top
+         nums.append(len(both))
+         idx.append(j)
+    nums = np.array(nums)
+    indx = np.argsort(nums*-1)[:K2]
+    sp=set()
+    for ii in indx:
+        idx_ = idx[ii]
+        sp = sp|set(data[idx_][:K1+1])
+    for  j in sp:
+         S[i][j] = 1.
+         S[j][i] = 1.
+sio.savemat('S_K1_'+str(K)+'_K2_'+str(K2)+'.mat',{'S':S})
+
